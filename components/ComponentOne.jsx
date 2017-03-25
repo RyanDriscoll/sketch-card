e79722d9060f8c2ce823f3d42e42eb0cc094f51f
Waseem999 <wakarim2@gmail.com>
2017-03-25 15:27:31 -0500

    working on frame

--- components/ComponentOne.jsx
+++ components/ComponentOne.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
-const nextCard = () => (
+const nextCard = (props) => (
       <div>
           <p>
               BATTER: 
-              <button type="button" className="btn btn-primary btn-sm"><span className="glyphicon glyphicon-arrow-left"></span></button>
+              <button type="button" className="btn btn-primary btn-sm"><span className="glyphicon glyphicon-arrow-left" onClick={props.subtractBatter} value={'subtract'}></span></button>
                <button type="button" className="btn btn-primary btn-sm"><span className="glyphicon glyphicon-menu-up"></span></button>
-               <button type="button" className="btn btn-primary btn-sm"><span className="glyphicon glyphicon-arrow-right"></span></button>
+               <button type="button" className="btn btn-primary btn-sm"><span className="glyphicon glyphicon-arrow-right" onClick={props.addBatter} value={'add'}></span></button>
           </p>
           <p>
               INNING: 
-              <button type="button" className="btn btn-primary btn-sm"><span className="glyphicon glyphicon-arrow-left"></span></button>
+              <button type="button" className="btn btn-primary btn-sm"><span className="glyphicon glyphicon-arrow-left" onClick={props.subtractInning} ></span></button>
                <button type="button" className="btn btn-primary btn-sm"><span className="glyphicon glyphicon-menu-up"></span></button>
-               <button type="button" className="btn btn-primary btn-sm"><span className="glyphicon glyphicon-arrow-right"></span></button>
+               <button type="button" className="btn btn-primary btn-sm"><span className="glyphicon glyphicon-arrow-right" onClick={props.addInning} ></span></button>
           </p>
       </div>
 );
--- components/SingleView.jsx
+++ components/SingleView.jsx
@@ -5,15 +5,50 @@ import DisplayInfo from './DisplayInfo.jsx';
 
 class SingleView extends React.Component {
 
+    constructor(props){
+    super(props)
+      this.state = {
+        batter: 1,
+        inning: 1,
+        data: null
+      }
+     this.addBatter = this.addBatter.bind(this);
+     this.subtractBatter = this.subtractBatter.bind(this);
+     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
     componentDidMount() {
     
     }
+
+    handleSubmit(){
+        let dataURL = canvas.toDataURL();
+        console.log(dataURL);
+    }
+
+    addBatter(e){
+        console.log("BATTER", this.state.batter)
+        this.setState({
+        batter: this.state.batter++,
+    })
+     console.log("BATTER", this.state.batter);
+     this.handleSubmit();
+    }
+
+    subtractBatter(e){
+        console.log("e", e)
+        this.setState({
+        batter: this.state.batter--,
+        })
+        
+    }
+
     render() {
         return (
         <div>
-            <DisplayInfo batter={2} inning={2}/>
+            <DisplayInfo batter={this.state.batter} inning={this.state.inning}/>
             <Frame/>
-            <ComponentOne/>
+            <ComponentOne addBatter={this.addBatter} subtractBatter={this.subtractBatter}/>
         </div>
         );
     }
